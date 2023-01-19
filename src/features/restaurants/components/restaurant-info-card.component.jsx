4df7a308fd7276f0abfd9a76c77693154edc00fa
lkowalik04@gmail.com <lkowalik04@gmail.com>
2023-01-19 20:08:11 +0100

    spacer

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -5,6 +5,7 @@ import { Text, StyleSheet, View, Image } from 'react-native';
 import { Avatar, Button, Card } from 'react-native-paper';
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
+import { Spacer } from '../../../components/spacer.component';
 
 const Title = styled.Text`
   font-family: ${(props) => props.theme.fonts.heading};
@@ -68,9 +69,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <View style={{ paddingLeft: 16 }} />
+            <Spacer variant="left.medium" />
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <View style={{ marginRight: 16 }} />
+            <Spacer variant="left.large" />
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
